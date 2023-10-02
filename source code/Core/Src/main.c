d699c658c5f6aea3ec2accea8a2c789ac7da3479
HUYTRAN\DELL <huyhoa0502@gmail.com>
2023-10-02 12:42:01 +0700

    timerRun code

--- source code/Core/Src/main.c
+++ source code/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "software_timer.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -94,11 +94,17 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  setTimer1(200);
   while (1)
   {
+	  if (timer1_flag == 1){
+		  setTimer1(200);
+		  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+	  }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
   }
   /* USER CODE END 3 */
 }
@@ -208,13 +214,15 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-int counter = 100;
+//10ms
+//int counter = 100;
 void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef * htim ) {
-	counter--;
-	if (counter <= 0) {
-		counter = 100;
-		HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-	}
+//	counter--;
+//	if (counter <= 0) {
+//		counter = 100;
+//		HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+//	}
+	timerRun();
 }
 /* USER CODE END 4 */
 
